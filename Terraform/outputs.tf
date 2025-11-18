b1c4d97f8ee363236c04f601fc42a39eff8207e6
Aviral Shukla <118339025+aviralshukla12@users.noreply.github.com>
2025-11-19 04:34:52 +0530

    Delete Terraform directory

--- Terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "server_public_ip" {
-  value = aws_instance.mern_server.public_ip
-}
-
-output "server_public_dns" {
-  value = aws_instance.mern_server.public_dns
-}
--- Terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_region    = "us-east-1"
-instance_type = "t2.medium"
-key_name      = "Ngioskey"  # Change this to your key pairs!
